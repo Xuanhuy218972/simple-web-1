d3a8c385462763683f6d23f7ef8eddba864f69ee
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-09-03 10:54:17 +0700

    Setup lib/frack.rb

---
+++ lib/frack/frack.rb
@@ -0,0 +1,21 @@
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+require 'rack'
+require 'tilt'
+require 'erb'
+require 'active_record'
+require 'pg'
+require 'bcrypt'
+
+ActiveRecord::Base.establish_connection(
+  adapter:  'postgresql',
+  host:     'localhost',
+  database: 'simpleweb1',
+  username: 'users', 
+  password: 'Xuanhuy21@nba' 
+)
+
+module Frack
+  autoload :Application, 'frack/application'
+  autoload :BaseController,  'frack/base_controller'
+  autoload :Router, 'frack/router'
+end
